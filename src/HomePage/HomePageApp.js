32063fc9c55fa58eb6bcf5880c39a7046abec1d3
Shiri <shiri3847@gmail.com>
2024-02-05 23:32:02 +0200

    add the posts from the json

--- src/HomePage/HomePageApp.js
+++ src/HomePage/HomePageApp.js
@@ -1,28 +1,14 @@
-
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Share from './components/Share';
 import Search from './components/Search';
 import PostsContainer from './components/PostsContainer';
 import Rightmenu from './components/Rightmenu';
-import PostItem from './components/PostItem';
 import './HomePageApp.css';
-
+import PostsData from './Posts.json';
 function HomePageApp() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState(PostsData);
   const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    // Fetch initial posts from JSON file
-    fetch('Posts.json')
-      .then(response => response.json())
-      .then(data => {
-        // Create a deep copy of the data before setting it to state
-        const copiedData = JSON.parse(JSON.stringify(data));
-        setPosts(copiedData);
-      })
-      .catch(error => console.error('Error fetching posts:', error));
-  }, []);
 
   const handleLike = (postId) => {
     setPosts(posts.map(post => {
@@ -40,7 +26,8 @@ function HomePageApp() {
   const addPost = (newPost) => {
     setPosts([newPost, ...posts]);
   };
-   const deletePost = (postId) => {
+  
+  const deletePost = (postId) => {
     setPosts(posts.filter(post => post.id !== postId));
   };
 
@@ -49,11 +36,13 @@ function HomePageApp() {
       <Header />
       <div className="main-content">
         <div className="left-content">
-          <Search darkMode={darkMode} /> {/* Pass darkMode to Search */}
-          <Share onPost={addPost} darkMode={darkMode} />{/* Pass darkMode to Search */}
-          <PostsContainer posts={posts} onLike={handleLike} onDelete={deletePost} darkMode={darkMode} />{/* Pass darkMode to Search */}
+          <Search darkMode={darkMode} />
+          <Share onPost={addPost} darkMode={darkMode} />
+         
+          <PostsContainer posts={posts} onLike={handleLike} onDelete={deletePost} darkMode={darkMode} />
+        
         </div>
-        <Rightmenu darkMode={darkMode} toggleDarkMode={toggleDarkMode} /> {/* Pass darkMode and toggleDarkMode to Rightmenu */}
+        <Rightmenu darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App'; 
+
 import './index.css'
-import FirstScreen from './FirstScreen';
-import HomePageApp from './HomePage/HomePageApp';
+import FirstScreen from './FirstScreen.js';
+import HomePageApp from './HomePage/HomePageApp.js';
 ReactDOM.render(
   <React.StrictMode>
     <HomePageApp /> {/* Render the main application component */}
