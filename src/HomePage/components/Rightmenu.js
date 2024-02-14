1eadd8a2e18b0f6e3407bbde8af85ab588bef8e9
Shiri <shiri3847@gmail.com>
2024-02-14 13:07:19 +0200

    change css

--- src/HomePage/components/Rightmenu.js
+++ src/HomePage/components/Rightmenu.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import './Rightmenu.css';
 import logo from './facebook_logo.png';
-
+import { Link } from 'react-router-dom';
 
 
 function RightMenu({ darkMode, toggleDarkMode, profilePhoto, onPhotoChange }) {
   const handleModeToggle = () => {
     toggleDarkMode();
   };
+  const fileInputRef = useRef(null);
 
   const handlePhotoChange = (e) => {
     const file = e.target.files[0];
@@ -20,35 +21,50 @@ function RightMenu({ darkMode, toggleDarkMode, profilePhoto, onPhotoChange }) {
     }
   };
 
+
+  const handleGalleryButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.removeAttribute('accept');
+      fileInputRef.current.click();
+    }
+  };
+
+ 
   return (
     <aside className={`right-menu ${darkMode ? 'dark-mode' : ''}`}>
       <div>
       <img src={logo} alt="Facebook Logo" className="logo" />
       </div>
       <img src={profilePhoto} alt="Profile" className="profile-picture-user" />
-
-      <ul>
+     
+      <button className="gallery-button" onClick={handleGalleryButtonClick}>
+        Choose from Gallery
+      </button>
       <input
+        ref={fileInputRef}
         id="profile-picture-upload"
         type="file"
         accept="image/*"
+        capture="camera"
         onChange={handlePhotoChange}
         className="choose-pic"
+        style={{ display: 'none' }}
       />
+      <ul>
         <li>Profile</li>
         <li>Posts</li>
         <li>Friends</li>
         <li>Groups</li>
       </ul>
       
-      
-      
       <button className="mode-toggle" onClick={handleModeToggle}>
         {darkMode ? 'Light Mode' : 'Dark Mode'}
       </button>
+      <p><Link to="/login">Log Out</Link></p>
     </aside>
+     
   );
-}
 
+  };
 export default RightMenu;
 
