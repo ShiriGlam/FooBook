49280c6c823b9587bda3f469add115b0e7e4a58f
Shiri <shiri3847@gmail.com>
2024-03-03 00:39:14 +0200

    odelete menu

--- src/HomePage/components/DropdownMenu.js
+++ src/HomePage/components/DropdownMenu.js
@@ -9,7 +9,7 @@ function DropdownMenu({ onDeleteAccount }) {
   return (
     <div className="dropdown-menu">
       <button onClick={handleDeleteAccount}>Delete Account</button>
-      {/* Add other menu items here */}
+      {}
     </div>
   );
 }
